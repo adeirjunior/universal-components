e64b0044d3d91344f1e44ff7e9841b4b65593ca9
Adeir Junior <73009242+adeirjunior@users.noreply.github.com>
2025-03-29 03:01:15 -0400

    fix error on export Card

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import './components/my-card';
 import './components/todo-item';
 
 export { TodoItem } from './components/todo-item/index';
-export { Card } from './components/my-card';
+export { MyCard } from './components/my-card';
